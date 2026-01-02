2306cd3a0e60daf217034ff10e482469c72f4108
Makiko <trimakichan@gmail.com>
2026-01-02 13:35:58 -0800

    Add Card and CardList components to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { CardList } from './components/CardList'
 
 function App() {
 
   return (
     <>
     <h1>Welcome To Inspiration Board!</h1>
+    <CardList />
     </>
   )
 }
---
+++ src/components/CardList.jsx
@@ -0,0 +1,23 @@
+import Card from './Card'
+
+const SAMPLE_DATA = [
+  "Eat pizza in Naples",
+  "Visit the Colosseum in Rome",
+  "Take a gondola ride in Venice",
+  "See the Vatican Museums",
+  "Explore art museums in Florence",
+  "Visit the Leaning Tower of Pisa",
+  "Drive along the Amalfi Coast",
+  "Go wine tasting in Tuscany"
+];
+
+export const CardList = () => {
+  return (
+    <>
+        <div>CardList</div>
+        <Card />
+    </>
+
+    
+  )
+}
