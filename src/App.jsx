07d819b0b8361dfc5e4eba587a7d0679d1dd0a64
Makiko <trimakichan@gmail.com>
2026-01-03 10:46:34 -0800

    Added a rule to ESLint and install proptypes. Working on layout in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ function App() {
   return (
     <>
     <h1>Welcome To Inspiration Board!</h1>
-    <CardList />
+    <div className='container'>
+    <div className="col-1">Boards</div>
+    <div className="col-2"><CardList /></div>
+    <div className="col-3">Forms</div>
+    </div>
     </>
   )
 }
