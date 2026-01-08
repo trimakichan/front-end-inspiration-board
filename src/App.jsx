90899a5991e591b73d750798625a5e806d4f82d9
Makiko <trimakichan@gmail.com>
2026-01-08 10:34:45 -0800

    refactor like and remove funtion in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import BoardList from './components/BoardList';
 import CardList from './components/CardList';
 import NewCardForm from './components/NewCardForm';
 import NewBoardForm from './components/NewBoardForm';
-import { SAMPLE_DATA } from './data/sample_data';
 
 const kbaseURL = 'https://back-end-inspiration-board-7juo.onrender.com';
 
@@ -26,19 +25,42 @@ const getAllBoardsAPI = () => {
     .catch(error => console.log(error));
 };
 
-const removeCardAPI = (boardId, cardId) => {
-  return axios.delete(`${kbaseURL}/cards/${cardId}`)
-    .catch(error => console.log(error));
-};
-
 const likeCardAPI = (cardId) => {
   return axios.patch(`${kbaseURL}/cards/${cardId}`)
     .catch(error => console.log(error));
 };
 
 const likeCard = card => {
-  return { ...card, likes: card.countLikes + 1 };
+  return { ...card, likes: card.likes + 1 };
 };
+
+const updateLikedCard = (boardId, cardId, boardData) => (
+  boardData?.id === boardId
+    ? {
+      ...boardData,
+      cards: boardData.cards.map(card =>
+        card.id === cardId
+          ? likeCard(card)
+          : card
+      )
+    }
+    : boardData
+);
+
+
+const removeCardAPI = (boardId, cardId) => {
+  return axios.delete(`${kbaseURL}/cards/${cardId}`)
+    .catch(error => console.log(error));
+};
+
+const getUpdatedBoardsAfterRemoval = (boardId, cardId, boardData) => (
+  boardData.id === boardId
+    ? {
+      ...boardData,
+      cards: boardData.cards.filter(card => card.id !== cardId)
+    }
+    : boardData
+);
 
 
 function App() {
@@ -47,19 +69,12 @@ function App() {
   const [selectedBoard, setSelectedBoard] = useState()
   const hideAllForms = !showForms.boardForm && !showForms.cardForm;
 
-
   useEffect(() => {
     getAllBoardsAPI().then(boards => {
       // const newBoards = boards.map(convertFromAPI);
       setBoardData(boards);
     });
   }, []);
-
-  useEffect(() => {
-    console.log('board data from app: ', boardData)
-
-  }, [boardData])
-
 
   const toggleShowForm = (formType) => {
     setShowForms(prev => ({
@@ -73,68 +88,35 @@ function App() {
     setSelectedBoard(board)
   };
 
-const handleRemoveCard = (boardId, cardId) => {
-  return removeCardAPI(boardId, cardId)
-    .then(() => {
+  const handleRemoveCard = (boardId, cardId) => {
+    return removeCardAPI(boardId, cardId)
+      .then(() => {
 
-      setBoardData(boardData =>
-        boardData.map(board =>
-          board.id === boardId
-            ? {
-                ...board,
-                cards: board.cards.filter(card => card.id !== cardId)
-              }
-            : board
-        )
-      );
+        setBoardData(prevBoardData =>
+          prevBoardData.map(board =>
+            getUpdatedBoardsAfterRemoval(boardId, cardId, board)
+          )
+        );
 
-      setSelectedBoard(selectedBoardData =>
-        selectedBoardData?.id === boardId
-          ? {
-              ...selectedBoardData,
-              cards: selectedBoardData.cards.filter(
-                card => card.id !== cardId
-              )
-            }
-          : selectedBoardData
-      );
+        setSelectedBoard(prevSelectedBoardData =>
+          getUpdatedBoardsAfterRemoval(boardId, cardId, prevSelectedBoardData)
+        );
 
-    });
-};
-
+      }).catch(e => console.log(e));;
+  };
 
   const handleLikeCard = (boardId, cardId) => {
     return likeCardAPI(cardId)
       .then(() => {
-        setBoardData(boardData =>
-          boardData.map(board =>
-            board.id === boardId
-              ? {
-                ...board,
-                cards: board.cards.map(card =>
-                  card.id === cardId
-                    ? likeCard(card)
-                    : card
-                )
-              }
-              : board
-          )
+        setBoardData(prevBoardData =>
+          prevBoardData.map(board => updateLikedCard(boardId, cardId, board))
         );
 
-        setSelectedBoard(selectedBoardData =>
-          selectedBoardData?.id === boardId
-            ? {
-              ...selectedBoardData,
-              cards: selectedBoardData.cards.map(card =>
-                card.id === cardId
-                  ? { ...card, likes: card.likes + 1 }
-                  : card
-              )
-            }
-            : selectedBoardData
+        setSelectedBoard(prevSelectedBoardData =>
+          updateLikedCard(boardId, cardId, prevSelectedBoardData)
         );
 
-      });
+      }).catch(e => console.log(e));;
   };
 
   const handleBoardSubmit = data => {
@@ -147,7 +129,6 @@ const handleRemoveCard = (boardId, cardId) => {
   };
 
   const handleCardSubmit = (boardId, data) => {
-    // console.log('board id', boardId, 'data', data)
     axios.post(`${kbaseURL}/boards/${boardId}/cards`, data)
       .then(results => {
         const newCard = results.data;
@@ -191,7 +172,6 @@ const handleRemoveCard = (boardId, cardId) => {
     </>
   )
 };
-
 
 export default App;
 
